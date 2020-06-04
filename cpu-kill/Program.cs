cc1df817a12f10803c8eb93415fab4dd1ca9663d
Daniel Ramos <ramos.danielferreira@gmail.com>
2020-06-04 15:10:12 -0300

    Teste de estresse simples

---
+++ cpu-kill/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace cpu_kill
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int percentage = 80;
+            for (int i = 0; i < Environment.ProcessorCount; i++)
+            {
+                (new Thread(() =>
+                {
+                    Stopwatch watch = new Stopwatch();
+                    watch.Start();
+                    while (true)
+                    {
+                        // Make the loop go on for "percentage" milliseconds then sleep the 
+                        // remaining percentage milliseconds. So 40% utilization means work 40ms and sleep 60ms
+                        if (watch.ElapsedMilliseconds > percentage)
+                        {
+                            Thread.Sleep(100 - percentage);
+                            watch.Reset();
+                            watch.Start();
+                        }
+                    }
+                })).Start();
+            }
+        }
+    }
+}
